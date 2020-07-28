9fc16d605d65391db351e1668018110c57280776
Shubham Gupta <shubhamgupta5893@gmail.com>
2020-07-28 20:15:18 +0530

    Delete blockchain.py

--- zi/blockchain.py
+++
@@ -1,6 +0,0 @@
-class Block():
-    def __init__(self,transactions):
-        self.transactions = transactions
-        self.merkle_tree = [] #### given the list of transactions, create their hashes and get the
-        
-        
